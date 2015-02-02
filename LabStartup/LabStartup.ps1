263c02dd42edce2a7df73da3a8c0e3fe05e7cf5c
bcall58 <bcall58@comcast.net>
2015-02-02 13:39:52 -0600

    tightened up start nested

--- LabStartup/LabStartup.ps1
+++ LabStartup/LabStartup.ps1
@@ -147,61 +147,51 @@ Function Start-Nested ( [array] $records ) {
 		# If blank, default to the first/only vCenter
 		If( $vcenter -eq $null ) { $vcenter = $vCenters[0] }
 		
-		# start vApps
 		If( $vApp = Get-VApp -Name $name -Server $vcenter -ea 0 ) {
+			$type = "vApp"
+			$startCMD = "Start-VApp -VApp $vApp -RunAsync -Server $vcenter"
+			$getCMD = "Get-VApp $name -Server $vcenter"
 			$powerState = [string]$vApp.Status
-			Write-Output $("Checking vApp {0} power state: {1}" -f $vApp.Name, $powerState )
-			While ( !($powerState.Contains("Started")) ) {
-				If ( !($powerState.Contains("Starting")) ) {
-					Write-Output $("Starting vApp {0} on {1}" -f $vApp.Name, $vcenter )
-					$task = Start-VApp -VApp $vApp -RunAsync -Server $vcenter	
-				}
-				LabStartup-Sleep $sleepSeconds
-				$task = Get-Task -Id $task.Id -Server $vcenter
-				$vApp = Get-VApp $name -Server $vcenter
-				$powerState = [string]$vApp.Status
-				If ($task.State -eq "Error") {
-						Write-Progress "FATAL ERROR" 'FAIL-2'  
-						$currentRunningSeconds = Get-RuntimeSeconds $startTime
-						$currentRunningMinutes = $currentRunningSeconds / 60
-						Write-Output $("FAILURE: labStartup ran for {0:N0} minutes and has been terminated."  -f $currentRunningMinutes )
-						Write-Output $("Cannot start vApp {0} on {1}" -f $vApp.Name, $vcenter )
-						Write-Output $("Error task Id {0} task state: {1}" -f $task.Id, $task.State )
-						Exit
-				}
-				Write-Output $("Current vApp {0} power state: {1}" -f $vApp.Name, $powerState )
-				Write-Output $("Current task Id {0} task state: {1}" -f $task.Id, $task.State )
-			}
+			$goodPower = "Started"
 		} ElseIf( $vm = Get-VM -Name $name -Server $vcenter -ea 0 ) {
+			$type = "vVM"
+			$startCMD = "Start-VM -VM $vm -RunAsync -Server $vcenter"
+			$getCMD = "Get-VM $name -Server $vcenter"
 			$powerState = [string]$vm.PowerState
-			Write-Output $("Checking vVM {0} power state: {1}" -f $vm.Name, $powerState )
-			While ( !($powerState.Contains("PoweredOn")) ) {
-				If ( !($powerState.Contains("Starting")) ) {
-					Write-Output $("  Starting vVM {0} on {1}" -f $vm.Name, $vcenter )
-					$task = Start-VM -VM $vm -RunAsync -Server $vcenter
+			$goodPower = "PoweredOn"
+		} Else {
+			Write-Output $("ERROR: Unable to find entity {0} on {1}" -f $name, $vcenter )
+			Continue
+		}
+		
+		Write-Output $("Checking {0} {1} power state: {2}" -f $type, $name, $powerState )
+		While ( !($powerState.Contains($goodPower)) ) {
+			If ( !($powerState.Contains("Starting")) ) {
+				Write-Output $("Starting {0} {1} on {2}" -f $type, $name, $vcenter )
+				$task = Invoke-Expression $startCMD
+				If ( $type -eq "vVM" ) {
 					$tasks = Get-Task -Server $vcenter
 					ForEach ($task in $tasks) {
 						If ($task.ObjectId -eq $vm.Id) { Break }
 					}
-					LabStartup-Sleep $sleepSeconds
-					$vm = Get-VM $name -Server $vcenter
-					$task = Get-Task -Id $task.Id -Server $vcenter
-					$powerState = [string]$vm.PowerState
-					Write-Output $("Current vVM {0} power state: {1}" -f $vm.Name, $powerState )
-					Write-Output $("Current task Id {0} task state: {1}" -f $task.Id, $task.State )
-					If ($task.State -eq "Error") {
-						Write-Progress "FATAL ERROR" 'FAIL-2'  
-						$currentRunningSeconds = Get-RuntimeSeconds $startTime
-						$currentRunningMinutes = $currentRunningSeconds / 60
-						Write-Output $("FAILURE: labStartup ran for {0:N0} minutes and has been terminated."  -f $currentRunningMinutes )
-						Write-Output $("Cannot start vVM {0} on {1}" -f $vm.Name, $vcenter )
-						Write-Output $("Error task Id {0} task state: {1}" -f $task.Id, $task.State )
-						Exit
-					}
 				}
 			}
-		} Else {
-			Write-Output $("ERROR: Unable to find entity {0} on {1}" -f $name, $vcenter )
+			LabStartup-Sleep $sleepSeconds
+			$task = Get-Task -Id $task.Id -Server $vcenter
+			$entity = Invoke-Expression $getCMD
+			If ( $type -eq "vApp") { $powerState = [string]$entity.Status }
+			Else { $powerState = [string]$entity.PowerState }
+			If ($task.State -eq "Error") {
+				Write-Progress "FATAL ERROR" 'FAIL-2'  
+				$currentRunningSeconds = Get-RuntimeSeconds $startTime
+				$currentRunningMinutes = $currentRunningSeconds / 60
+				Write-Output $("FAILURE: labStartup ran for {0:N0} minutes and has been terminated."  -f $currentRunningMinutes )
+				Write-Output $("Cannot start {0} {1} on {2}" -f $type, $name, $vcenter )
+				Write-Output $("Error task Id {0} task state: {1}" -f $task.Id, $task.State )
+				Exit
+			}
+			Write-Output $("Current {0} {1} power state: {2}" -f $type, $name, $powerState )
+			Write-Output $("Current task Id {0} task state: {1}" -f $task.Id, $task.State )
 		}
 	}
 } #End Start-Nested
