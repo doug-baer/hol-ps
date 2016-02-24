35e926e342f03683e5a6de7cb13eca9deee1b5cc
Doug Baer <dbaer@vmware.com>
2016-02-24 13:30:46 -0700

    created vPodChecker.ps1 version 1.0

--- vPodChecker/vPodChecker.ps1
+++ vPodChecker/vPodChecker.ps1
@@ -1,65 +1,80 @@
 <#
 
-.SYNOPSIS
+.SYNOPSIS			This script is intended to run some standard config validation
+							checks for Hands-on Labs vPods. It also attempts to remediate
+							some simple and common misconfigurations like uuid.action and
+							keyboard.typematicMinDelay on Linux vVMs
 
-.DESCRIPTION
+.DESCRIPTION	Check vPod configuration and remediate some misconfigurations
 
-.NOTES
+.NOTES				Requires PowerCLI -- tested with v6.0u1
+							Version 1.0 - 24 Februrary 2016
  
-.EXAMPLE
+.EXAMPLE			.\vPodChecker.ps1
 
-.INPUTS
+.INPUTS				None
 
-.OUTPUTS
+.OUTPUTS			Interactive: all output is written to console
 
 #>
 
-##############################################################################
-##### User Variables
-##############################################################################
+#####Check SSL certificates on PROVIDED links
+## USER must provide this list based on what is in your pod
+## Start by pulling this array from the labStartup file. We'll filter out non-https links
+$URLs = @{
+	'https://vcsa-01a.corp.local:9443/vsphere-client/' = 'vSphere Web Client'
+	'http://stga-01a.corp.local/account/login' = 'FreeNAS'
+	'https://psc-01a.corp.local/' = 'Platform'
+	}
 
-# It would be nice to read vCenters, etc. from the LabStartup.ps1 but it is pretty tricky to do.
-# If we separate out the arrays into a PS Module then it would be much easier
-#$labStartup = 'C:\hol\LabStartup.ps1'
+$urlsToTest = $URLs.Keys | where { $_ -match 'https' } 
 
-# Credentials used to login to vCenters (in vSphere 6 special license administrator privilege must be used)
-$vcuser = 'administrator@vsphere.local'
-$password = 'VMware1!'
-
-$sleepSeconds = 10
-
-
-# HOL licenses should not expire before this date
-$chkDate2 = Get-Date "12/31/2015 12:00:00 AM"
-
-# HOL licenses should expire before this date
-$chkDate2 = Get-Date "01/02/2016 12:00:00 AM"
-
-$licensePass = $true
-
-#must be defined in order to pass as reference for looping
-$result = ''
-
-##############################################################################
-
-
-#FQDN of vCenter server(s)
+#FQDN(s) of vCenter server(s)
 $vCenters = @(
 	'vcsa-01a.corp.local'
 	#'vcsa-02a.corp.local'
 )
 
+# Credentials used to log in to all vCenters 
+# (in vSphere 6, account with 'license administrator' privilege must be used)
+$vcuser = 'administrator@vsphere.local'
+$password = 'VMware1!'
+
+$sleepSeconds = 10
+
+# HOL licenses should NOT expire before this date
+$chkDateMin = Get-Date "01/01/2016 12:00:00 AM"
+
+# HOL licenses should expire before this date
+$chkDateMax = Get-Date "01/28/2017 12:00:00 AM"
+
+$licensePass = $true
+
+#must be defined in order to pass as reference for looping during connect
+$result = ''
+
+#Certificate Validation
+$minValidDate = [datetime]"12/31/2016"
+$ExtraCertDetails = $false
+
+
 ##############################################################################
 
-#Load the VMware PowerCLI tools
+#Load the VMware PowerCLI tools ... need to revise this for PowerCLI 6.0+
 Try {
-  Add-PSSnapin VMware.VimAutomation.Core -ErrorAction 1
-  Add-PSSnapin VMware.VimAutomation.License -ErrorAction 1  
+	Add-PSSnapin VMware.VimAutomation.Core -ErrorAction 1
+	Add-PSSnapin VMware.VimAutomation.License -ErrorAction 1	
 } 
 Catch {
 	Write-Host "No PowerCLI found, unable to continue."
-	Exit
+#	Exit
 }
+
+#Disable SSL certificate validation checks... it's a Lab!
+$scvc = [System.Net.ServicePointManager]::ServerCertificateValidationCallback
+[System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
+
+##############################################################################
 
 Function Connect-VC ([string]$server, [string]$username, [string]$password, [REF]$result) {
 <#
@@ -67,77 +82,297 @@ Function Connect-VC ([string]$server, [string]$username, [string]$password, [REF
 	It sets the $result variable to 'success' or 'fail' based on the result
 #>
 	Try {
-		Connect-ViServer -server $server -username $username -password $password -ea 1
+		Connect-ViServer -server $server -username $username -password $password -ErrorAction 1 | Out-Null
 		Write-Host "Connection Successful"
 		$result.value = "success"
 	}
 	Catch {
 		Write-Host "Failed to connect to server $server"
-#		Write-Host $_.Exception.Message
+		Write-Host $_.Exception.Message
 		$result.value = $false
 	}
 } #End Connect-VC
 
-##############################################################################
-##### Main Script - Base vPod
-##############################################################################
 
-# connect to each vCenter and then evaluate licensing
-# 
+Function Test-TcpPort ([string]$server, [int]$port) {
+	Try {
+		$socket = New-Object Net.Sockets.TcpClient
+		$socket.Connect($server,$port)
+		if($socket.Connected) {
+			Write-Host "Successfully connected to server $server on port $port"
+			return $true
+		}
+	}
+	Catch {
+		Write-Host "Failed to connect to server $server on port $port"
+		Return $false
+	}
+	$socket.Dispose()
+} #End Test-TcpPort
+
+
+##############################################################################
+##### Connect to each vCenter
+##############################################################################
 
 Foreach ($vcserver in $vCenters) {
 	Do {
 		Connect-VC $vcserver $vcuser $password ([REF]$result)
 		Start-Sleep $sleepSeconds
 	} Until ($result -eq "success")
-	
-	#check for evaluation licenses in use
-	$LM= Get-view LicenseManager
-    $LAM= Get-View $LM.LicenseAssignmentManager 
-    $param = @($null)
-    $assets = $LAM.GetType().GetMethod("QueryAssignedLicenses").Invoke($LAM,$param)
+}
 
-	Foreach ($asset in $assets) {
-	    if ( $asset.AssignedLicense.LicenseKey -eq '00000-00000-00000-00000-00000' ) {
-		  # special case - make certain nothing is in evaluation mode
-		    $name = $asset | Select-Object -ExpandProperty EntityDisplayName
-		    Write-Host "Please check EVALUATION assignment on $name!" -foregroundcolor "red"
-			$licensePass = $false
+##############################################################################
+##### Check and report SSL Certificates
+##############################################################################
+Write-Host "==== SSL CERTIFICATES ===="
+$sslReport = @()
+
+foreach( $url in $urlsToTest ) {
+
+	If( $url -like "https*" ) {
+		#write-host $url
+		$h = [regex]::Replace($url, "https://([a-z\.0-9\-]+).*", '$1')
+		If( ($url.Split(':') | Measure-Object).Count -gt 2 ) {
+			$p = [regex]::Replace($url, "https://[a-z\.0-9\-]+\:(\d+).*", '$1')
+		} Else { $p =	443 }
+		#Write-Host $h on port $p
+
+		If( $ExtraCertDetails ) {
+			$item = "" | select HostName, PortNum, CertName, Thumbprint, Issuer, EffectiveDate, ExpiryDate, DaysToExpire
+		} Else {
+			$item = "" | select HostName, PortNum, CertName, ExpiryDate, DaysToExpire, Issuer
 		}
-	}
-	
-	# query the license expiration
-	Foreach ($License in ($LM | Select -ExpandProperty Licenses)) {
-	    if ( !($License.LicenseKey -eq '00000-00000-00000-00000-00000') ) {
-		  $VC = ([Uri]$LM.Client.ServiceUrl).Host
-		  $Name = $License.Name
-		  $lKey = $License.LicenseKey
-		  $used = $License.Used
-		  $labels = $License.Labels | Select -ExpandProperty Value
-		  $expDate = $License.Properties | Where-Object {$_.Key -eq "expirationDate"} | Select-Object -ExpandProperty Value
-		  if ( ($expDate -ge $chkDate1) -and ($expDate -le $chkDate2) ) {
-             #Write-Host "License $Name $lKey is good and expires $expDate"
-			 if ( $used -eq 0 ) {
-			   Write-Host "License $Name is UNASSIGNED and MUST be removed." -foregroundcolor "red"
-			   $licensePass = $false
-			 }
-		  } else {
-		     Write-Host "License $Name $lKey is BAD. It expires $expDate"
-			 $licensePass = $false
-		  }
-		  # need to make certain expDate is AFTER chkDate
+
+		$item.HostName = $h
+		$item.PortNum = $p
+
+		If (Test-TcpPort $h $p ) {
+			#Get the certificate from the host
+			$wr = [Net.WebRequest]::Create("https://$h" + ':' + $p)
+		
+			#The following request usually fails for one reason or another:
+			# untrusted (self-signed) cert or untrusted root CA are most common...
+			# we just want the cert info, so it usually doesn't matter
+			Try { 
+				$response = $wr.GetResponse() 
+				#This sometimes results in an empty certificate... probably due to a redirection
+				If( $wr.ServicePoint.Certificate ) {
+					If( $ExtraCertDetails ) {
+						$t = $wr.ServicePoint.Certificate.GetCertHashString()
+						$SslThumbprint = ([regex]::matches($t, '.{1,2}') | %{$_.value}) -join ':'
+						$item.Thumbprint = $SslThumbprint
+						$item.EffectiveDate = $wr.ServicePoint.Certificate.GetEffectiveDateString()
+					}
+					$cn = $wr.ServicePoint.Certificate.GetName()
+					$item.CertName = $cn.Replace('CN=',';').Split(';')[-1].Split(',')[0]
+					$item.Issuer = $wr.ServicePoint.Certificate.Issuer
+					$item.ExpiryDate = $wr.ServicePoint.Certificate.GetExpirationDateString()
+					$validTime = New-Timespan -End $item.ExpiryDate -Start $minValidDate
+					If( $validTime.Days -lt 0 ) {
+						$item.DaysToExpire = "$validTime.Days - *** EXPIRES EARLY *** "
+					} Else {
+						$item.DaysToExpire = $validTime.Days
+					}
+				}
+			}
+			Catch{
+				Write-Host "Unable to get certificate for $h on $port"
+			}
+			Finally {
+				if( $response ) {
+					$response.Close()
+					Remove-Variable response
+				}
+			}
+			$sslReport += $item
 		}
 	}
 }
+$sslReport | FT -AutoSize
+Write-Host "=========================="
+
+
+##############################################################################
+##### Check Host Settings
+##############################################################################
+Write-Host "==== HOST CONFIGURATION - NTP ===="
+$hostReport = @()
+
+##### NTP is configured
+
+$allhosts = Get-VMHost
+foreach ($h in $allhosts) {
+	$row = "" | Select HOSTNAME, NTPDRUNNING, NTPDPOLICY, NTPSERVER
+	$row.HOSTNAME = $h.name
+	$ntpData = Get-VMHostService -VMHost $h	| where { $_.key -eq 'ntpd' }
+	$row.NTPDRUNNING =	$ntpData.Running
+	$row.NTPDPOLICY	=	$ntpData.Policy
+	$row.NTPSERVER	 =	Get-VMHostNtpServer -VMHost $h
+	$hostReport += $row
+}
+$hostReport | ft -auto
+Remove-Variable row
+Write-Host "=========================="
+
+
+##############################################################################
+##### Check vVM Settings
+##############################################################################
+Write-Host "==== L2 VM CONFIGURATION ===="
+$vmReport = @()
+
+##### Report/correct UUID.action setting on vVMs
+
+$allvms = Get-VM
+foreach ($vm in $allvms) {
+	$row = "" | Select VMNAME,OSTYPE,UUIDACTION,TYPEDELAY
+	$row.VMNAME = $vm.name
+	
+	$currentUuidAction = Get-AdvancedSetting -en $vm -name uuid.action
+	$currentUuidActionValue = $currentUuidAction.Value
+	if( $currentUuidActionValue -eq "keep" ) {
+		$row.UUIDACTION = $currentUuidActionValue
+	} elseif(! $currentUuidActionValue ) {
+		try {
+			New-AdvancedSetting -en $vm -name uuid.action -value 'keep' -Confirm:$false -ErrorAction 1 | Out-Null
+			$row.UUIDACTION = "was BLANK"
+		} catch {
+			Write-Host -Fore Red	"Failed to create UUID.action on $($vm.name)"
+			$row.UUIDACTION = "FIXMANUAL"
+		}
+	} else {
+		try {
+			Set-AdvancedSetting $currentUuidAction -value 'keep' -Confirm:$false -ErrorAction 1
+			$row.UUIDACTION = "was $currentUuidActionValue"
+		} catch {
+			Write-Host -Fore Red	"Failed to set UUID.action on $($vm.name)"
+			Write-Host -Fore Red	"	value remains: $((Get-AdvancedSetting -en $vm -name uuid.action).value)"
+			$row.UUIDACTION = "FIXMANUAL"
+		}
+	}
+	
+	##### Report/correct typematic delay... for Linux machines only
+	
+	$row.OSTYPE = $vm.GuestId
+	if( $vm.GuestId -match 'linux|ubuntu|debian|centos|sles|redhat|other' ) {
+		$currentTypeDelay = Get-AdvancedSetting -en $vm -name keyboard.typematicMinDelay
+		$currentTypeDelayValue = $currentTypeDelay.Value
+		if( $currentTypeDelayValue -eq 2000000 ) {
+			$row.TYPEDELAY = $currentTypeDelayValue
+		} elseif(! $currentTypeDelay ) {
+			try {
+				New-AdvancedSetting -en $vm -name keyboard.typematicMinDelay -value 2000000 -Confirm:$false -ErrorAction 1 | Out-Null
+				$row.TYPEDELAY = "was BLANK"
+			} catch {
+				Write-Host -Fore Red	"Failed to create keyboard.typematicMinDelay on $($vm.name)"
+				$row.TYPEDELAY = "FIXMANUAL"
+			}
+
+		} else {
+			try {
+				Set-AdvancedSetting $currentTypeDelay -value 2000000 -Confirm:$false -ErrorAction 1
+				$row.TYPEDELAY = "was $currentTypeDelayValue"
+			} catch {
+				Write-Host -Fore Red "Failed to set keyboard.typematicMinDelay on $($vm.name)"
+				Write-Host -Fore Red	"	value remains: $((Get-AdvancedSetting -en $vm -name keyboard.typematicMinDelay).value)"
+				$row.TYPEDELAY = "FIXMANUAL"
+			}
+		}
+	}
+	$vmReport += $row
+}
+
+$vmReport | ft
+Remove-Variable row
+Write-Host "=========================="
+
+
+
+##############################################################################
+##### Check Licensing
+##############################################################################
+
+Write-Host "==== VCENTER LICENSES ===="
+$licenseReport = @()
+
+#check for evaluation licenses in use
+$LM = Get-View LicenseManager
+$LAM = Get-View $LM.LicenseAssignmentManager 
+$param = @($null)
+$assets = $LAM.GetType().GetMethod("QueryAssignedLicenses").Invoke($LAM,$param)
+
+
+foreach ($asset in $assets) {
+	if ( $asset.AssignedLicense.LicenseKey -eq '00000-00000-00000-00000-00000' ) {
+		# special case - make certain nothing is in evaluation mode
+		$name = $asset | Select-Object -ExpandProperty EntityDisplayName
+		Write-Host "Please check EVALUATION assignment on $name!" -foregroundcolor "red"
+		$licensePass = $false
+	}
+}
+	
+# query the license expiration for all installed licenses
+
+foreach( $License in ($LM | Select -ExpandProperty Licenses) ) {
+	if ( !($License.LicenseKey -eq '00000-00000-00000-00000-00000') ) {
+		$row = "" | Select LICENSENAME,LICENSEKEY,EXPIRATION,STATUS
+		$VC = ([Uri]$LM.Client.ServiceUrl).Host
+		$Name = $License.Name
+		$lKey = $License.LicenseKey
+		$used = $License.Used
+		$labels = $License.Labels | Select -ExpandProperty Value
+
+		$row.LICENSENAME = $Name
+		$row.LICENSEKEY = $lKey
+		
+		$expDate = $License.Properties | Where-Object {$_.Key -eq "expirationDate"} | Select-Object -ExpandProperty Value
+
+		if( $expDate ) { 
+			$row.EXPIRATION = $expDate
+		} else {
+			$row.EXPIRATION = 'NEVER'
+		}
+
+		if( $expDate -and (($expDate -ge $chkDateMin) -and ($expDate -le $chkDateMax)) ) {
+			#Write-Verbose "License $Name $lKey is good and expires $expDate"
+			$row.STATUS = 'GOOD'
+			if( $used -eq 0 ) {
+				Write-Host "License $Name is UNASSIGNED and should be removed." -foregroundcolor "yellow"
+				$row.STATUS = 'UNASSIGNED'
+				$licensePass = $false
+			}
+		} else {
+			if( ! $expDate ) {
+				Write-Host "License $Name $lKey NEVER expires!!" -foregroundcolor "red"
+			} else {
+				Write-Host "License $Name $lKey is BAD. It expires $expDate"
+				$row.STATUS = 'EXPIRING'
+			}
+			$licensePass = $false
+		}
+		# need to make certain expDate is AFTER chkDate
+	}
+	$licenseReport += $row
+}
+
+$licenseReport | ft
+
+if( $licensePass ) {
+	Write-Host "Well done! Final result of license check is PASS" -foregroundcolor "green"
+}
+else {
+	Write-Host "Final result of license check is FAIL" -foregroundcolor "red"
+}
+Remove-Variable row
+Write-Host "=========================="
+
+##### Disconnect from all vCenters
 
 Foreach ($vcserver in $vCenters) {
 	Write-Output "$(Get-Date) disconnecting from $vcserver ..."
 	Disconnect-VIServer -Server $vcserver -Confirm:$false
 }
 
-If ( $licensePass ) {
-    Write-Host "Well done!  Final result of license check is PASS" -foregroundcolor "green"
-}
-Else {
-    Write-Host "Final result of license check is FAIL" -foregroundcolor "red"
-}
+
+###### END ######
