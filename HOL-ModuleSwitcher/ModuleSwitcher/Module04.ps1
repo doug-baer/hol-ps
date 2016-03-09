9c401aa17c3df978e2835a6b86ffa6a6c4670ae4
Doug Baer <dbaer@vmware.com>
2016-03-09 12:49:49 -0700

    reorganization/rename - version 1.0

---
+++ HOL-ModuleSwitcher/ModuleSwitcher/Module04.ps1
@@ -0,0 +1,9 @@
+if( $args[0] -eq 'START' ) {
+	#TODO: Write your Module START code here
+	Write-Host "Doing Module 4 Start stuff"
+} else {
+	#TODO: Write your Module STOP code here
+	Write-Host "Doing Module 4 Stop stuff"
+}
+
+Pause
---
+++ HOL-ModuleSwitcher/ModuleSwitcher/Module05.ps1
@@ -0,0 +1,9 @@
+if( $args[0] -eq 'START' ) {
+	#TODO: Write your Module START code here
+	Write-Host "Doing Module 5 Start stuff"
+} else {
+	#TODO: Write your Module STOP code here
+	Write-Host "Doing Module 5 Stop stuff"
+}
+
+Pause
