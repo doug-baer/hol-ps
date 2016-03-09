c2179df1b4e9d2ec5ddfba83b283ac388375c73c
Doug Baer <dbaer@vmware.com>
2016-03-09 09:46:10 -0700

    better Module## template

--- ModuleSelector/TEMPLATE-Module02.ps1
+++ ModuleSelector/TEMPLATE-Module02.ps1
@@ -1,29 +1,55 @@
 #
-#  Module02.ps1 - Module 2 Start/Stop Script
+#  Module02.ps1 - Module 2 Start/Stop Script EXAMPLE
 #
 
 #Path to HOL directory in the vPod
 $holPath = 'C:\HOL'
-#This module number -- for output clarity
+
+#This script's module number -- for output clarity
 $moduleNumber = 2
+
+#some of the labstartup functions need a temp variable to communicate status
+$result = ''
 
 Function ModuleStart {
 	$startTime = $(Get-Date)
 	Write-Host "Beginning Module $moduleNumber START @ $StartTime"
 	
 	LoadPowerCLI
-
-	# include the HOL LabStartupFunctions
+	#LoadLabStartupFunctions
 	$InvocationPath = Join-Path $holPath 'LabStartupFunctions.ps1'
 	if( Test-Path $InvocationPath ) {
+		Write-Host -ForegroundColor Green "Loading functions from $InvocationPath"
 		. $InvocationPath
-		Write-Verbose "Loading functions from $InvocationPath"
 	} else {
-		Write-Verbose -Fore Red "ERROR: Unable to find $InvocationPath"
+		Write-Host -ForegroundColor Red "ERROR: Unable to find $InvocationPath"
 		Break
 	}
-	
+		
 	#TODO: Write your Module START code here
+
+	$mod2VMs = ('db-01a:vcsa-01a.corp.local','web-01a:vcsa-01a.corp.local')
+	
+	Connect-VC 'vcsa-01a.corp.local' 'administrator@corp.local' 'VMware1!' ([REF]$result)
+
+	foreach( $record in $mod2VMs ) {
+		($name,$vcenter) = $record.Split(":")
+		$vm = Get-VM $name -server $vcenter
+		if( $vm.PowerState -eq 'PoweredOn' ) {
+			Write-Host "VM $name is already running."
+		} else {
+			Write-Host "VM $name is starting."
+			Start-VM -VM $vm -Server $vcenter -Confirm:$false -RunAsync | Out-Null
+			Sleep -Sec 10
+		}
+	}
+	
+	Write-Host "Activating the Chaos Monkey..."
+	#You can do other things you need here, too
+	Start-Sleep -Seconds 5
+	
+	Write-Host "Disconnecting from vCenter(s)"
+	Disconnect-Viserver * -Confirm:$false
 	
 	Write-Host $( "$(Get-Date) Finished. Runtime was {0:N0} minutes." -f  ((Get-RuntimeSeconds $startTime) / 60) )
 	
@@ -36,12 +62,51 @@ Function ModuleStop {
 	Write-Host "Beginning Module $moduleNumber STOP @ $StartTime"
 
 	LoadPowerCLI
+	#LoadLabStartupFunctions
+	$InvocationPath = Join-Path $holPath 'LabStartupFunctions.ps1'
+	if( Test-Path $InvocationPath ) {
+		Write-Host -ForegroundColor Green "Loading functions from $InvocationPath"
+		. $InvocationPath
+	} else {
+		Write-Host -ForegroundColor Red "ERROR: Unable to find $InvocationPath"
+		Break
+	}
 	
 	#TODO: Write your Module STOP code here
+	
+	$mod2VMs = ('db-01a:vcsa-01a.corp.local','web-01a:vcsa-01a.corp.local')
+	
+	Connect-VC 'vcsa-01a.corp.local' 'administrator@corp.local' 'VMware1!' ([REF]$result)
+
+	foreach( $record in $mod2VMs ) {
+		($name,$vcenter) = $record.Split(":")
+		$vm = Get-VM $name -server $vcenter
+		if( $vm.PowerState -eq 'PoweredOff' ) {
+			Write-Host "VM $name is already powered off."
+		} else {
+			if( $vm.ExtensionData.Guest.ToolsRunningStatus -eq 'guestToolsRunning' ) {
+				Write-Host "VM $name is shutting down."
+				Stop-VMGuest -VM $vm -Server $vcenter -Confirm:$false | Out-Null
+				Sleep -Sec 20
+			} else {
+				Write-Host "VM $name is being powered off."
+				Stop-VM -VM $vm -server $vcenter -RunAsync -Confirm:$false | Out-Null
+			}
+		}
+	}
+
+	Write-Host "Deactivating the Chaos Monkey..."
+	#You can undo other things you need here, too
+	Start-Sleep -Seconds 5
+	
+	Write-Host "Disconnecting from vCenter(s)"
+	Disconnect-Viserver * -Confirm:$false
 
 	Write-Host $( "$(Get-Date) Finished. Runtime was {0:N0} minutes." -f  ((Get-RuntimeSeconds $startTime) / 60) )
 
 } #ModuleStop
+
+
 
 ##########################################################################
 # supporting functions
@@ -50,13 +115,14 @@ Function LoadPowerCLI {
 	Try {
 		#For PowerCLI v6.x
 		$PowerCliInit = 'C:\Program Files (x86)\VMware\Infrastructure\vSphere PowerCLI\Scripts\Initialize-PowerCLIEnvironment.ps1'
+		Write-Host -ForegroundColor Green "Loading PowerCLI"
 		. $PowerCliInit
 	} 
 	Catch {
-		Write-Host "No PowerCLI found, unable to continue."
+		Write-Host -ForegroundColor Red "No PowerCLI found, unable to continue."
 		Break
 	} 
-}
+} #LoadPowerCLI
 
 ##########################################################################
 # The main program logic
