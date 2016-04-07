18cb9d8634f7d1cd59de32829b0fd1e2c4d1b0d6
Doug Baer <dbaer@vmware.com>
2016-04-07 14:11:34 -0700

    ConfigureStandaloneEsxiHost updated for 2016

--- vPod-Mgmt/ConfigureStandaloneEsxiHost.ps1
+++ vPod-Mgmt/ConfigureStandaloneEsxiHost.ps1
@@ -1,9 +1,22 @@
 <#
-	Configuration script for HOL vCenter(s) and vESXi hosts
-	Start with empty vCenter inventory
-	End with Datacenter, Cluster, $baseHostCount hosts in the Cluster
-		NFS datastore mounted, VDS implemented and hosts migrated to the VDS
+	Configuration script for HOL "extra" vESXi hosts
+		Basic networking + VSAN prep
+		
+	April 7, 2016
 #>
+
+
+$rootPassword = 'VMware1!'
+$ntpServer = '192.168.100.1'
+# Base config has a single 5 GB SSD and a single 40 GB HDD
+$vsanSsdSize = 5
+$vsanHddSize = 40
+
+#Change this to a or b, depending on which site's hosts are being built
+$site = 'a'
+#Set this to the numbers of the hosts that you want to configure
+$hostNumbers = (4,5,6,7,8)
+
 
 $esxSettings = @{
 	'UserVars.DcuiTimeOut' = 0
@@ -12,12 +25,23 @@ $esxSettings = @{
 	'Syslog.global.defaultRotate' = 2
 }
 
-#only valid setting if attached to vCenter
-# 	'Vpx.Vpxa.Config.log.level' = 'info'
+#Import the module we use for flagging SSD/HDD devices
+try {
+	Import-Module 'C:\HOL\Tools\HOL-SCSI.psm1' -ErrorAction 1
+} catch {
+	Write-Host "no HOL-SCSI module"
+}
 
 
-$hostNames = $('esx-03a.corp.local','esx-04a.corp.local')
-$rootPassword = 'VMware1!'
+#####################################################################
+
+$hostNames = @()
+foreach( $hostNumber in $hostNumbers ) { 
+	if( $hostNumber -lt 10 ) { 
+		$hostNumber = "0$hostNumber" 
+	}
+	$hostNames += ("esx$hostNumber" + "$site.corp.local") 
+}
 
 #####################################################################
 
@@ -25,14 +49,27 @@ Foreach( $hostName in $hostNames) {
 	Write-Host -Fore Green "Working on host $hostname"
 	Connect-VIserver $hostName -username root -password $rootPassword
 	$h = Get-VmHost $hostname
-	#Configure NTP on the host
-	Add-VMhostNtpserver -vmhost $h -ntpserver '192.168.100.1'
+	#get the short host name (first node of FQDN)
+	$hn = ($hostname.Split('.'))[0]
+
+	Write-Host -Fore Green "Working on NTP"
+	Add-VMhostNtpserver -vmhost $h -ntpserver $ntpServer
 	Get-VMHostFirewallException  -vmh $h | where {$_.name -like "*NTP Client*" } | Set-VMHostFirewallException -Enabled:$true
 	Get-VMHostService -vmhost $h | Where {$_.key -eq "ntpd"} | Start-VMHostService
-	Get-VMHostService -vmhost $h | Where {$_.key -eq "ntpd"} | Set-VMHostService -policy 'automatic'
-	
-	#Basic vSwitch (vSS) Configuration to get access to the NFS storage
+	Get-VMHostService -vmhost $h | Where {$_.key -eq "ntpd"} | Set-VMHostService -policy 'on'
+
+
+	Write-Host -Fore Green "Working on Networking"	
+	##Basic vSwitch (vSS) Configuration
 	$vs = get-virtualswitch -name vSwitch0 -vmhost $h
+
+	#Add vmnic1 to the vSwitch0 so it has two uplinks
+	$pNic1 = $h | Get-VMHostNetworkAdapter -Physical -Name vmnic1
+
+	$vswitch = Get-VirtualSwitch -VMHost $h -Standard -Name "vSwitch0"
+	$vswitch | Add-VirtualSwitchPhysicalNetworkAdapter -VMHostPhysicalNic $pNic1 -Confirm:$false
+
+	#Add vmkernel ports for vmotion and virtual SAN
 	$matches = Select-String -InputObject $h.Name -Pattern 'esx-(\d+)(a|b).corp.local'
 	$hostNum = [int]$matches.Matches[0].Groups[1].value
 	$hostSite = $matches.Matches[0].Groups[2].value
@@ -40,29 +77,59 @@ Foreach( $hostName in $hostNames) {
 	$hostID = 50 + $hostNum
 	$storageIP = '10.00.20.' + $hostID
 	$vmotionIP = '10.00.30.' + $hostID
-	$nfsHostIP = '10.00.20.60'
+	$vmotionGW = '10.00.30.1'
+
 	If( $hostSite -eq 'a' ) {
 		$storageIP = $storageIP.Replace('00','10')
 		$vmotionIP = $vmotionIP.Replace('00','10')
-		$nfsHostIP = $nfsHostIP.Replace('00','10')
+		$vmotionGW = $vmotionGW.Replace('00','10')
 	} Else {
 		$storageIP = $storageIP.Replace('00','20')
 		$vmotionIP = $vmotionIP.Replace('00','20')
-		$nfsHostIP = $nfsHostIP.Replace('00','20')
+		$vmotionGW = $vmotionGW.Replace('00','20')
 	}
-		
-	New-VmhostNetworkAdapter -vmhost $h -portgroup $($hostname +"_storage") -IP $storageIP -subnetmask 255.255.255.0 -virtualswitch $vs -confirm:$false
-	New-VmhostNetworkAdapter -vmhost $h -portgroup $($hostname +"_vmotion") -IP $vmotionIP -subnetmask 255.255.255.0 -virtualswitch $vs -vmotionenabled:$true -confirm:$false
-		
-	#Add vmnic1 to the vSwitch0 so it has two uplinks
-	$pNic1 = $h | Get-VMHostNetworkAdapter -Physical -Name vmnic1
-	Get-VirtualSwitch -VMHost $h -Standard -Name "vSwitch0" | Add-VirtualSwitchPhysicalNetworkAdapter -VMHostPhysicalNic $pNic1 -Confirm:$false
 	
-	#Tweak advanced settings on the host
-	#can I get away with just setting the 'global' value instead of resetting each one?
+	#vmk1 is vmotion
+	#This command uses the default IP stack in ESXi
+	#New-VMhostNetworkAdapter -vmhost $h -portgroup $($hn +"_vmotion") -IP $vmotionIP -subnetmask 255.255.255.0 -virtualswitch $vs -vmotionenabled:$true -confirm:$false
+	#This block creates the vmk1 interface on the 'vmotion' IP stack
+	$vmotion_pg = New-VirtualPortGroup -Name $($hn +"_vmotion") -VirtualSwitch $vswitch
+	$nic = New-Object VMware.Vim.HostVirtualNicSpec
+	$nic.Portgroup = $vmotion_pg
+	$nic.netStackInstanceKey = 'vmotion'
+
+	$ip = New-Object VMware.Vim.HostIpConfig
+	$ip.dhcp = $false
+	$ip.ipAddress = $vmotionIP
+	$ip.subnetMask = '255.255.255.0'
+	$nic.Ip = $ip
+
+	$networkSystem = $h.ExtensionData.configManager.NetworkSystem
+	$theHostNetworkSystem = Get-view -Id ($networkSystem.Type + "-" + $networkSystem.Value)
+	$theHostNetworkSystem.AddVirtualNic($vmotion_pg.name, $nic)
+
+	#Set the default route for the vmotion stack
+	$esxcli = Get-EsxCli -VMHost $h
+	$esxcli.network.ip.route.ipv4.add($vmotionGW,'vmotion','default')
+	
+	#vmk2 is VSAN, flag it for VSAN traffic	
+	New-VmHostNetworkAdapter -vmhost $h -portgroup $($hn +"_storage") -IP $storageIP -subnetmask 255.255.255.0 -virtualswitch $vs -vsantrafficenabled $true -confirm:$false
+
+	Write-Host -Fore Green "Working on Advanced Settings"	
 	Get-AdvancedSetting -Entity $h -Name 'Syslog.loggers.*.rotate' | Set-AdvancedSetting -Value 2 -Confirm:$false | Out-Null
 	Foreach ($setting in $esxSettings.Keys) { 
 		Get-AdvancedSetting -Entity $h -name $setting | Set-AdvancedSetting -Value $($esxSettings[$setting]) -Confirm:$false | Out-Null
+	}
+	
+	## Configure the LUNs for VSAN (assume all 'SSD' devices are the same size
+	if( Get-Module HOL-SCSI ) {
+		Write-Host -Fore Green "Working on Storage"
+		Get-SCSILun | where {$_.CapacityGB -eq $vsanSsdSize} | Set-ScsiLunFlags | Out-Null
+		Get-SCSILun | where {$_.CapacityGB -eq $vsanHddSize} | Set-ScsiLunFlags -ExplicitHDD | Out-Null
+		
+		Get-SCSILun | Get-ScsiLunFlags
+	} else {
+		Write-Host -Fore Yellow "No PS module HOL-SCSI loaded, not flagging devices"
 	}
 	
 	Write-Host -Fore Green "Finished with host $hostname"
--- vPod-Mgmt/EmptyMyRecycleBin.ps1
+++
@@ -1,6 +0,0 @@
-# Empty recycle bin for current user
-$Shell = New-Object -ComObject Shell.Application
-$RecBin = $Shell.Namespace(0xA)
-If( $RecBin.Items().Count -gt 0 ) {
-	$RecBin.Items() | %{Remove-Item $_.Path -Recurse -Confirm:$false}
-}
