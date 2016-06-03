474f2b6b1c1a7a4040e3a351f0cfb48562e9e061
Doug Baer <dbaer@vmware.com>
2016-06-03 13:38:13 -0700

    updated ModuleSwitcher example script for Module 1

--- HOL-ModuleSwitcher/ModuleSwitcher/EXAMPLES/HOL-1700-1/Module01.ps1
+++ HOL-ModuleSwitcher/ModuleSwitcher/EXAMPLES/HOL-1700-1/Module01.ps1
@@ -1,6 +1,6 @@
 if( $args[0] -eq 'START' ) {
-	#The Module 1 START action is never called.
-	#This functionality should be handled by C:\HOL\LabStartup.ps1
+	#TODO: Write your Module START code here
+	Write-Host "Starting Module 1 for HOL-1700-MBL-1"
 } else {
 	#TODO: Write your Module STOP code here
 	Write-Host "Stopping Module 1 for HOL-1700-MBL-1"
